7c5df83426db4b0f04b637353fdee48ddf493222
Jingo88 <jcory.ng@gmail.com>
2015-01-16 15:50:04 -0500

    split new file

---
+++ new.js
@@ -0,0 +1,11 @@
+var url = "http://api.randomuser.me/"
+var xhr = new XMLHttpRequest();
+xhr.addEventListener('load', function(e) {
+    var d = xhr.responseText
+    var parsed = JSON.parse(d)
+    var person = parsed['results'][0]['user']['email']
+    console.log(person);
+    // response["results"][num]["user"]["email"]
+})
+xhr.open("GET", url);
+xhr.send();
