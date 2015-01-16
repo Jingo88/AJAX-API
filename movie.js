60ed44d413df73f3b92fb476cfd6c7c46f6ae284
Jingo88 <jcory.ng@gmail.com>
2015-01-16 15:46:07 -0500

    first day with AJAX and APIs

---
+++ movie.js
@@ -0,0 +1,25 @@
+var submit = document.querySelector('button')
+var input = document.querySelector('input')
+submit.addEventListener('click', function() {
+    findPoster();
+});
+input.addEventListener('keyup', function(e) {
+    if (e.keyCode === 13) {
+        findPoster();
+    }
+})
+
+function findPoster() {
+    var movie = input.value
+    var movie_url = encodeURI(movie)
+    var url = "http://omdbapi.com/?t=" + movie_url
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', function(e) {
+        var d = xhr.responseText
+        var parsed = JSON.parse(d)
+        var img = document.querySelector('img');
+        img.src = parsed.Poster;
+    })
+    xhr.open("GET", url);
+    xhr.send();
+}
